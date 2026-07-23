ddbb8b71d47f88b8e1decacb9fb03007826f9455
Parth <89581958+Parth-demo@users.noreply.github.com>
2026-07-24 00:15:20 +0530

    Change greeting message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hi, World!");
+        System.out.println("Hey, World! This is the second commit.");
     }
 }
